efeee55c27c54415059d640c88a35cdca8b66635
罗子扬 <13662597+lzywantsleep@user.noreply.gitee.com>
2026-01-06 10:17:11 +0800

    一些东西忘了提交

---
+++ src/test/java/com/lzy/codestudybackend/MainApplicationTests.java
@@ -0,0 +1,19 @@
+package com.lzy.codestudybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 主类测试
+ *
+ * @author lzy
+ * 
+ */
+@SpringBootTest
+class MainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
